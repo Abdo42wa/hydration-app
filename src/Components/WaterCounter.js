1ce46354f9d2f7d4fa2644734099519296a0d56b
Abdo Elsayed <abdo.ahmed.aa754@gmail.com>
2022-04-06 12:32:49 +0300

    Update WaterCounter.js
    added daily reset function for the cups amount and updated variable name

--- src/Components/WaterCounter.js
+++ src/Components/WaterCounter.js
@@ -2,32 +2,45 @@ import { useEffect, useState } from "react";
 import React from 'react'
 
 const WaterCounter = () => {
-    const [cup, setCup] = useState(0);
-    const [message, setMessage] = useState("")
+    const [cupsAmount, setCupsAmount] = useState(0);
+    const [message, setMessage] = useState("");
+
+    var today = new Date();
 
     const addWater = () => {
 
-        if (cup !== 8) {
+        if (cupsAmount !== 8) {
 
             var cupsNumber = Number(localStorage.getItem('cupsNumber'));
 
-            setCup(cup + 1);
+            setCupsAmount(cupsAmount + 1);
             localStorage.setItem('cupsNumber', ++cupsNumber);
+            localStorage.setItem('Date', today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate())
         } else {
 
             setMessage("Oops you have reached your limit ");
         }
 
     }
-    useEffect(() => {
-        setCup(Number(localStorage.getItem('cupsNumber')))
+    const getTheCurentDay = () => {
 
+        var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        if (currentDate != localStorage.getItem('Date')) {
+            localStorage.setItem('cupsNumber', 0)
+        } else {
+            return
+        }
+
+    }
+    useEffect(() => {
+        setCupsAmount(Number(localStorage.getItem('cupsNumber')))
+        getTheCurentDay();
     }, []);
 
     return (
         <>
             <p>drink water( 2L - 4L)</p>
-            <h2>{cup}</h2>
+            <h2>{cupsAmount}</h2>
             <p style={{ color: 'red' }}>{message}</p>
             <button onClick={() => addWater()}>Add cup</button>
 

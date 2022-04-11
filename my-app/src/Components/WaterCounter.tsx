6927cdc5b47aa28e2976b31af281bb596b4abe31
Abdo Elsayed <abdo.ahmed.aa754@gmail.com>
2022-04-11 10:08:56 +0300

    updated the UI to use material UI

--- my-app/src/Components/WaterCounter.tsx
+++ my-app/src/Components/WaterCounter.tsx
@@ -1,52 +1,56 @@
 import React, { useEffect, useState } from 'react'
+import { Typography, Input, Alert } from '@mui/material';
+import Button from '@mui/material/Button'
 const MAX_WATER_AMOUNT = 4000;
-//const MIN_WATER_AMOUNT = 2000;
 const WaterCounter = () => {
     const [waterAmount, setWaterAmount] = useState(0);
     const [message, setMessage] = useState("");
+    const [userInput, setUserInpute] = useState(0);
 
     const addWater = () => {
-
-        if (waterAmount <= MAX_WATER_AMOUNT) {
-            localStorage.setItem('waterAmount', `${waterAmount}`);
+        const result = waterAmount + userInput;
+        if (result <= MAX_WATER_AMOUNT) {
+            localStorage.setItem('waterAmount', `${result}`);
             localStorage.setItem('Date', new Date().toLocaleDateString('lt-LT'));
+            return;
+        }
+        //setMessage("Oops you have reached your limit ");
+    }
+
+    const onWaterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        //let result = Number(localStorage.getItem('waterAmount')) + Number(e.target.value)
+        const result = waterAmount + userInput;
+        setUserInpute(Number(e.target.value));
+        if (result <= MAX_WATER_AMOUNT) {
+            setMessage("");
             return;
         }
         setMessage("Oops you have reached your limit ");
     }
 
-    const onWaterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-        let result = Number(localStorage.getItem('waterAmount')) + +(e.target.value)
-        setWaterAmount(result);
-    }
-
-    const getCurentDay = () => {
+    const setCurentDay = () => {
 
         var currentDate = new Date().toLocaleDateString('lt-LT');
         if (currentDate !== localStorage.getItem('Date')) {
-            localStorage.setItem('cupsNumber', String(0))
-            console.log(currentDate);
-            console.log(localStorage.getItem('Date'))
-            return null;
+            localStorage.setItem('cupsNumber', `${0}`)
         }
-
     }
 
     useEffect(() => {
-        setWaterAmount(Number(localStorage.getItem('waterAmount')))
-        getCurentDay();
+        setWaterAmount(Number(localStorage.getItem('waterAmount')));
+        setCurentDay();
     }, []);
 
 
     return (
         <>
-            <p>Drink water( 2L - 4L)</p>
-            <h2>{waterAmount} ml</h2>
-            <label>Enter water in milliliters</label>
-            <input type='number' onChange={(e) => onWaterChange(e)} />
-            {message && <p style={{ color: 'red' }}>{message}</p>}
-            <button onClick={() => addWater()}>Add cup</button>
+            <Typography>Drink water( 2L - 4L)</Typography>
+            <Typography fontWeight={900} component={'h2'} >{waterAmount + userInput} ml</Typography>
+            <Typography>Enter water in milliliters</Typography>
+            <Input type='number' onChange={onWaterChange} />
+            {message && <Alert severity="error">{message}</Alert>}
+            <Button variant="outlined" color="success" size="small" onClick={() => addWater()}>Add cup</Button>
         </>
     )
 }
